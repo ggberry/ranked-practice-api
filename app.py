611fcec7275c1517b96741f0c04bff3ca69dd44f
ggberry <ggsoleil97@gmail.com>
2026-03-03 21:46:33 -0500

    Removed app.run

--- app.py
+++ app.py
@@ -79,5 +79,5 @@ def index():
     return '<span style="font-family: Consolas; font-size: 16px;">Ranked Practice seed API is running.</span>'
 
 # RUN:
-app.run(debug=True)
+# app.run(debug=True)
 # gunicorn app:app --bind 0.0.0.0:$PORT
